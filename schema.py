9ad5d2a8bcb962bfb423be55750abdf3e06fbb9a
Brian J King <brianjking@users.noreply.github.com>
2024-02-13 11:29:39 -0800

    updates to the schema to match Bobcat's media and activity types

--- schema.py
+++ schema.py
@@ -2,36 +2,6 @@ from pydantic import BaseModel, validator, ValidationError, constr
 from enum import Enum
 from typing import List
 
-class ActivityTypeEnum(str, Enum):
-    DirectMail = "DirectMail"
-    LocalAd = "LocalAd"
-    RegionalAd = "RegionalAd"
-    Handouts = "Hand-outs"
-    Billboard = "Billboard"
-    ExternalBrandingSignage = "External Branding Signage"
-    DealerDisplayAdvertising = "Dealer Display Advertising"
-    RadioAd = "RadioAd"
-    TVAd = "TVAd"
-    Tradeshows = "Tradeshows"
-    Exhibition = "Exhibition"
-    eBlast = "e-Blast"
-    MusicChannel = "Music Channel"
-    PaidListing = "Paid Listing"
-    OnlineDisplayAd = "Online Display Ad"
-    PaidSearch = "Paid Search"
-    SocialAd = "Social Ad"
-    SearchEngineOptimization = "Search Engine Optimization (SEO)"
-    BMISocialProgram = "BMI Social Program"
-    BMIEmailProgram = "BMI Email Program"
-    BMIPaidMedia = "BMI Paid Media"
-    CTV = "CTV"
-    Kenect = "Kenect"
-    FacilityUpgrades = "Facility Upgrades"
-    Sponsorship = "Sponsorship"
-    DealerSignage = "Dealer Signage"
-    VehicleWrapsDecals = "Vehicle Wraps/Decals"
-    Unknown = "Unknown"
-
 class MediaTypeEnum(str, Enum):
     Print = "Print"
     Outdoor = "Outdoor"
@@ -45,8 +15,36 @@ class MediaTypeEnum(str, Enum):
     Vehicle_Wraps = "Vehicle Wraps"
     Unknown = "Unknown"
 
+class ActivityTypeEnum(str, Enum):
+    DirectMail = "Direct Mail"
+    LocalAd = "Local Ad"
+    RegionalAd = "Regional Ad"
+    Handouts = "Hand-outs"
+    Billboards = "Billboards"
+    Signage = "Signage"
+    DealerDisplayAdvertising = "Dealer Display Advertising"
+    Television = "Television"
+    Radio = "Radio"
+    Tradeshows = "Tradeshows"
+    Exhibition = "Exhibition"
+    eBlast = "e-Blast"
+    MusicChannel = "Music Channel"
+    PaidListing = "Paid Listing"
+    OnlineDisplayAd = "Online Display Ad"
+    PaidSearch = "Paid Search"
+    SocialAd = "Social Ad"
+    SearchEngineOptimization = "Search Engine Optimization (SEO)"
+    BMISocialProgram = "BMI Social Program"
+    BMIPaidMedia = "BMI Paid Media"
+    CTV = "CTV"
+    Kenect = "Kenect"
+    FacilityUpgrades = "Facility Upgrades"
+    Sponsorship = "Sponsorship"
+    DealerSignage = "Dealer Signage"
+    VehicleWrapsDecals = "Vehicle Wraps/Decals"
+    Unknown = "Unknown"
+
 class CFM(BaseModel):
-    """Data model for a CFM clai."""
     vendor_merchant_name: str
     bill_invoice_amount: str
     date_of_invoice: str
@@ -62,11 +60,11 @@ class CFM(BaseModel):
         media_type = values.get('media_type')
         valid_activities = {
             MediaTypeEnum.Print: [ActivityTypeEnum.DirectMail, ActivityTypeEnum.LocalAd, ActivityTypeEnum.RegionalAd, ActivityTypeEnum.Handouts, ActivityTypeEnum.Unknown],
-            MediaTypeEnum.Outdoor: [ActivityTypeEnum.Billboard, ActivityTypeEnum.ExternalBrandingSignage, ActivityTypeEnum.Unknown],
+            MediaTypeEnum.Outdoor: [ActivityTypeEnum.Billboards, ActivityTypeEnum.Signage, ActivityTypeEnum.Unknown],
             MediaTypeEnum.Point_of_Purchase: [ActivityTypeEnum.DealerDisplayAdvertising, ActivityTypeEnum.Unknown],
-            MediaTypeEnum.Broadcast: [ActivityTypeEnum.RadioAd, ActivityTypeEnum.TVAd, ActivityTypeEnum.Unknown],
+            MediaTypeEnum.Broadcast: [ActivityTypeEnum.Television, ActivityTypeEnum.Radio, ActivityTypeEnum.Unknown],
             MediaTypeEnum.Events: [ActivityTypeEnum.Tradeshows, ActivityTypeEnum.Exhibition, ActivityTypeEnum.Unknown],
-            MediaTypeEnum.Digital: [ActivityTypeEnum.eBlast, ActivityTypeEnum.MusicChannel, ActivityTypeEnum.PaidListing, ActivityTypeEnum.OnlineDisplayAd, ActivityTypeEnum.PaidSearch, ActivityTypeEnum.SocialAd, ActivityTypeEnum.SearchEngineOptimization, ActivityTypeEnum.BMISocialProgram, ActivityTypeEnum.BMIEmailProgram, ActivityTypeEnum.BMIPaidMedia, ActivityTypeEnum.CTV, ActivityTypeEnum.Kenect, ActivityTypeEnum.Unknown],
+            MediaTypeEnum.Digital: [ActivityTypeEnum.eBlast, ActivityTypeEnum.MusicChannel, ActivityTypeEnum.PaidListing, ActivityTypeEnum.OnlineDisplayAd, ActivityTypeEnum.PaidSearch, ActivityTypeEnum.SocialAd, ActivityTypeEnum.SearchEngineOptimization, ActivityTypeEnum.BMISocialProgram, ActivityTypeEnum.BMIPaidMedia, ActivityTypeEnum.CTV, ActivityTypeEnum.Kenect, ActivityTypeEnum.Unknown],
             MediaTypeEnum.Facility_Branding: [ActivityTypeEnum.FacilityUpgrades, ActivityTypeEnum.Unknown],
             MediaTypeEnum.Sponsorships: [ActivityTypeEnum.Sponsorship, ActivityTypeEnum.Unknown],
             MediaTypeEnum.Signage: [ActivityTypeEnum.DealerSignage, ActivityTypeEnum.Unknown],
@@ -74,7 +72,7 @@ class CFM(BaseModel):
             MediaTypeEnum.Unknown: [ActivityTypeEnum.Unknown],
         }
         if media_type and v not in valid_activities.get(media_type, []):
-            return ActivityTypeEnum.Unknown
+            raise ValueError(f"Invalid activity type '{v}' for media type '{media_type}'")
         return v
 
 class Cocktail(BaseModel):
